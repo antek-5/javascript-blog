3eea1f8fc136658183bab2e3613b5d7ba3d5c649
Antoni Jankowski <antek.jankowski.jv@gmail.com>
2024-03-18 18:37:46 +0100

    Change js

--- js/script.js
+++ js/script.js
@@ -9,10 +9,12 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
-    console.log(event);
+    event.preventDefault();
+    const clickedElement = this;
+    
 
 
-    console.log('Link was clicked!');
+    
   
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -20,21 +22,31 @@ const titleClickHandler = function(event){
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-  
+    
     /* add class 'active' to the clicked link */
+    
+    clickedElement.classList.add('active');
+
+    
   
     /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.active a.active');
+    const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
-        activeArticle.classArticle.remove('active');
+        activeArticle.classList.remove('active');
     }
   
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    
   
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
   
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+    
   }
   
   const links = document.querySelectorAll('.titles a');
